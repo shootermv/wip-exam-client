da39468307e14d1b314594e945db112a8f6a8d88
shootermv <shootermv@gmali.com>
2018-07-16 18:06:51 +0300

    added logo instead of "company name"

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link , NavLink} from "react-router-dom"
 
 export default () => (
   <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-         <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+         <h5 className="my-0 mr-md-auto font-weight-normal"><img className="logo" src="assets/icon.png"/></h5>
          <nav  className="my-2 my-md-0 mr-md-3">
            
                 <NavLink className="btn" activeClassName="btn-primary" exact={true}  to="/">
