fedfdd290a316b83eefd2ee4b4deba75c2c0b1ff
Moshe Vilner <moshevilner@wipoferxps.webintpro.com>
2019-03-03 10:26:56 +0200

    prettifing the code of 'Users' component

--- src/home/users/Users.js
+++ src/home/users/Users.js
@@ -6,13 +6,19 @@ export default ({users, userSelected}) =>(
   
   <div className="col-md-4">
    {users.length === 0 && <Spinner/>}
-   {users.length > 0 && <div className="card"><ul className="list-group list-group-flush">
-      {users.map((user) => (
-        <li className="list-group-item" key={user.id} onClick={()=>userSelected(user)}>
-
-         <img className="user-img" src={user.profile_image_url}/> <div className="user-details"> {user.screen_name} <p> <a href={user.url}>visit</a> </p></div>
-      
-        </li>))}
-    </ul></div>}
+   {users.length > 0 && 
+   <div className="card">
+     <ul className="list-group list-group-flush">
+      {
+        users.map((user) => (
+        <li className="list-group-item" key={user.id} onClick={() => userSelected(user)}>
+           <img className="user-img" src={user.profile_image_url}/> 
+           <div className="user-details"> {user.screen_name} 
+              <p> <a href={user.url}>visit</a> </p>
+           </div>      
+        </li>))
+      }
+      </ul>
+    </div>}
   </div>
 )
